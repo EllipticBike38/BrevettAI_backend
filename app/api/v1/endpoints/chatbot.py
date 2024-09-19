9107209d9bff26528b72e6fee5361b02818d15ed
msara1111 <sara.metalli@gmail.com>
2024-09-19 08:16:11 +0200

    sostituire il token

--- app/api/v1/endpoints/chatbot.py
+++ app/api/v1/endpoints/chatbot.py
@@ -1,18 +1,35 @@
-from google_patent_scraper import scraper_class #type: ignore
-from fastapi import APIRouter, Depends
-from typing import List, Optional
-from crud.crud_patent import create_patent
-from sqlalchemy.orm import Session
-from db.session import get_db
+from fastapi import APIRouter
 import requests #type: ignore
+import json
+
 
 router = APIRouter()
 
-@router.get("/get_patents")
+
+ai_url = "https://api.regolo.ai/v1/chat/completions"
+
+
+@router.post("/get_patents")
 def get_patents(
+    patent: str
 ):
+    query = f"""
+    Sei un esperto di brevetti, non sai rispondere a domande che non riguardano i brevetti. 
+    Quando ti arriva un messaggio da un utente, contatti le API di https://patents.google.com/ e controlli se esiste già un brevetto uguale o simile.
+    Non restituire lo script con cui effettui la richiesta, ma solo il risultato di essa. 
+    La richiesta è la seguente: {patent}
+    """
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {TOKEN}"
+}
+    data = {
+    "model": "llama3.1:70b-instruct-q8_0",
+    "messages": [
+        {"role": "user", "content": query}
+    ]
+}
+    response = requests.post(ai_url, headers=headers, data=json.dumps(data))
 
-
-    response = requests.get("https://patents.google.com/xhr/query?url=q%3D(moka)%26oq%3Dmoka")
-    print(response.json())
-
+    response_json= response.json()
+    return json.dumps(response_json["choices"][0]["message"]["content"])
--- app/api/v1/endpoints/patents.py
+++ app/api/v1/endpoints/patents.py
@@ -4,14 +4,6 @@ from crud.crud_patent import create_patent
 from sqlalchemy.orm import Session
 from db.session import get_db
 
-'''
-salva un brevetto non tuo (l'utente manda l'id del brevetto preesistente)
-recupera tutti i brevetti salati (titoli)
-recupera il tuo singolo brevetto
-recupera il brevetto di un'altro (tramite query su google patents)
-elimina brevetto salvato
-'''
-
 
 router = APIRouter()
 
@@ -25,13 +17,3 @@ async def save_patent(
 ) -> None:
     create_patent(1, name, "pending", body, db)
 
-
-#update del brevetto tuo (l'utente aggiunge/modifica corpo e titolo del brevetto)
-# @router.post("/update_patent/")
-# async def update_patent(
-#         old_name: str,
-#         new_name: Optional[str],
-#         body: Optional[str]
-# ) -> None:
-#     for patent in patents:
-        
