1817c563b7a43b928d6899be4ff7dc30a11dea67
Andrea Mazzini <andrea.mazzini00@gmail.com>
2024-09-19 08:40:36 +0200

    Refactor CORS configuration in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from api.v1.endpoints import patents, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173",
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 # app.include_router(items.router, prefix="/items", tags=["items"])
